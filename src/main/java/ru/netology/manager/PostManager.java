62f29d298733ef4bbeac35032dd0def16d81e3e0
Alexandr-1221 <ags.sad.1221@gmail.com>
2021-02-14 19:27:51 +0300

    task 2

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int accountId, String domain, String query, String ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int accountId, int id){
+    }
+}
